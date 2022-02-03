747ca0857d20576396b855d9e70456dac6a2b030
arashioz <balali.arash@gmail.com>
2022-02-03 14:47:16 +0330

    Updates

---
+++ precache-manifest.bb8587f4c93a35fdd7663eb2bcabb91f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7226d5507d9ff1dbc953594f56cefef7",
+    "url": "/DeployApp/index.html"
+  },
+  {
+    "revision": "0c41285c0d9bde30e072",
+    "url": "/DeployApp/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0c41285c0d9bde30e072",
+    "url": "/DeployApp/static/js/2.d2b076ca.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/DeployApp/static/js/2.d2b076ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac07ffe80ecf18077ec",
+    "url": "/DeployApp/static/js/main.0f20481f.chunk.js"
+  },
+  {
+    "revision": "37450ffa84807bf4ce94",
+    "url": "/DeployApp/static/js/runtime-main.0dd9ee23.js"
+  },
+  {
+    "revision": "0ab26cfbc4c0b510f45d0456b9b600be",
+    "url": "/DeployApp/static/media/dropdown-icon.0ab26cfb.svg"
+  }
+]);
